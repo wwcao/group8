e63f3ff8dd3f48e9fa2441fda65b88eb4f4d3565
Weipeng Cao <wwcao@colorado.edu>
2015-11-11 13:51:41 -0700

    new data config

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=userDB',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'csci_proj',
+    'password' => 'csci_proj',
     'charset' => 'utf8',
 ];
