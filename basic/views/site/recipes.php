bced40fcfe1423c4c0b24c6a3c7d6c2a38b97293
Weipeng Cao <wwcao@colorado.edu>
2015-11-15 10:46:10 -0700

    recovered file

--- basic/views/site/recipes.php
+++ basic/views/site/recipes.php
@@ -4,11 +4,16 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Use these links to find delicious recipes!';
+$this->title = 'Use these links to find crafting information you need:';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-recipes">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1> Find Crafting Projects on The Following Websites: </h1>
+<p>
+</p>
+<p>
+</p>
+    <h3>Cooking and baking</h3>
 
     <p>
         <a href="http://allrecipes.com">Allrecipes</a>
@@ -26,11 +31,98 @@ $this->params['breadcrumbs'][] = $this->title;
       <p>
       <a href="http://www.simplyrecipes.com">Simply Recipes</a>
       </p>
-      
-      <p>
-		  <a href="http://www.foodnetwork.com">Food Network</a>
-      </p>
-      
 
     
 </div>
+
+<div class="site-recipes">
+    <h2>Woodworking and carving</h2>
+
+    <p>
+        <a href="http://www.finewoodworking.com/">Fine Woodworking</a>
+
+
+    </p>
+    <p>
+        <a href="http://www.popularwoodworking.com/">Popular Woodworking</a>
+
+     </p>
+     <p>
+         <a href="https://www.reddit.com/r/woodworking">Reddit: Woodworking</a>
+
+      </p>
+      <p>
+      <a href="http://www.woodmagazine.com/woodworking-plans/">Wood Magazine</a>
+      </p>
+
+
+</div>
+<div class="site-recipes">
+    <h2>Sewing, knitting, and crocheting</h2>
+
+    <p>
+        <a href="http://www.lionbrand.com/learnToKnit-fr.html">Lion Brand: Learn to Knit</a>
+
+
+    </p>
+    <p>
+        <a href="http://www.epicurious.com">Epicurious</a>
+
+     </p>
+     <p>
+         <a href="http://www.food.com">Food</a>
+
+      </p>
+      <p>
+      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
+      </p>
+
+    
+</div>
+<div class="site-recipes">
+    <h2>Fishing, hunting, and outdoorsmanship</h2>
+
+    <p>
+        <a href="http://allrecipes.com">Allrecipes</a>
+
+
+    </p>
+    <p>
+        <a href="http://www.epicurious.com">Epicurious</a>
+
+     </p>
+     <p>
+         <a href="http://www.food.com">Food</a>
+
+      </p>
+      <p>
+      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
+      </p>
+
+    
+</div>
+<div class="site-recipes">
+    <h1>Gardening, growing vegetables, and other plant-related activities:</h1>
+
+    <p>
+        <a href="http://allrecipes.com">Allrecipes</a>
+
+
+    </p>
+    <p>
+        <a href="http://www.epicurious.com">Epicurious</a>
+
+     </p>
+     <p>
+         <a href="http://www.food.com">Food</a>
+
+      </p>
+      <p>
+      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
+      </p>
+
+    
+</div>
+
+
+
