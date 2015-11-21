0b3e5eaca9610e548a5b28c395ec7ad458b80cc7
Weipeng Cao <wwcao@colorado.edu>
2015-11-20 20:06:31 -0700

    working on feather add to group and delete group

--- basic/views/site/view-group.php
+++ basic/views/site/view-group.php
@@ -9,10 +9,38 @@ use yii\widgets\LinkPager;
     <ul class="groups">
     <?php foreach ($myGroups as $group): ?>
         <li><div class="group">
-                <h4><?= Html::encode("{$group->groupname}") ?></h4>
-                <p>Created on <?= Html::encode("{$group->create_date}") ?></p>
+                <div style="text-align:center;">
+                    <h4><?= Html::encode("{$group->groupname}") ?></h4>
+                    <p>Created on <?= Html::encode("{$group->create_date}") ?></p>
+                </div>
+                <div style="text-align: center;">
+                    <?= Html::submitButton('Delete', ['class' => 'btn btn-primary']) ?>
+                </div>
             </div>
-            <br>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+
+    <?= LinkPager::widget(['pagination' => $pagination]) ?>
+</div>
+
+<?php }?>
+
+<?php if(count($myGroups)>0) { ?>
+<div>
+    
+    <h1>Joined Groups</h1>
+    <ul class="groups">
+    <?php foreach ($myGroups as $group): ?>
+        <li><div class="group">
+                <div style="text-align:center;">
+                    <h4><?= Html::encode("{$group->groupname}") ?></h4>
+                    <p>Created on <?= Html::encode("{$group->create_date}") ?></p>
+                </div>
+                <div style="text-align: center;">
+                    <?= Html::submitButton('Delete', ['class' => 'btn btn-primary']) ?>
+                </div>
+            </div>
         </li>
     <?php endforeach; ?>
     </ul>
