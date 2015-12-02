d8b664e9b0df80d9eb3db9e7948896ed7ae21327
Austin Linn <linnaj@colorado.edu>
2015-12-02 13:55:08 -0700

    finished recipes

--- basic/views/site/recipes.php
+++ basic/views/site/recipes.php
@@ -8,9 +8,9 @@ $this->title = 'Use these links to find crafting information you need:';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-recipes">
-<h1> Find Crafting Projects on The Following Websites: </h1>
+<h1 style="color:purple"> Find Crafting Projects on The Following Websites: </h1>
 
-    <h2>Cooking and baking</h2>
+    <h2 style="color:green">Cooking and baking</h2>
 
     <p>
         <a href="http://allrecipes.com">Allrecipes</a>
@@ -29,90 +29,73 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 <div class="site-recipes">
-    <h2>Woodworking and carving</h2>
+    <h2 style="color:green">Woodworking and carving</h2>
 
     <p>
-        <a href="http://www.finewoodworking.com/">Fine Woodworking</a>
-
-
-    </p>
+		<a href="http://www.finewoodworking.com/">Fine Woodworking</a>
+	</p>
     <p>
         <a href="http://www.popularwoodworking.com/">Popular Woodworking</a>
-
-     </p>
-     <p>
-         <a href="https://www.reddit.com/r/woodworking">Reddit: Woodworking</a>
-
-      </p>
-      <p>
-      <a href="http://www.woodmagazine.com/woodworking-plans/">Wood Magazine</a>
-      </p>
+	</p>
+	<p>
+		<a href="https://www.reddit.com/r/woodworking">Reddit: Woodworking</a>
+	</p>
+    <p>
+		<a href="http://www.woodmagazine.com/woodworking-plans/">Wood Magazine</a>
+    </p>
 
 
 </div>
 <div class="site-recipes">
-    <h2>Sewing, knitting, and crocheting</h2>
+    <h2 style="color:green">Sewing, knitting, and crocheting</h2>
 
     <p>
         <a href="http://www.lionbrand.com/learnToKnit-fr.html">Lion Brand: Learn to Knit</a>
-
-
     </p>
     <p>
-        <a href="http://www.epicurious.com">Epicurious</a>
-
-     </p>
-     <p>
-         <a href="http://www.food.com">Food</a>
-
-      </p>
-      <p>
-      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
-      </p>
+        <a href="http://fabricateboulder.com/">Fabric in Boulder</a>
+    </p>
+    <p>
+    	<a href="http://www.sewing.org/">Sewing.org</a>
+    </p>
+    <p>
+		<a href="http://www.knittingparadise.com/">Knitting and Crocheting</a>
+    </p>
 
     
 </div>
 <div class="site-recipes">
-    <h2>Fishing, hunting, and outdoorsmanship</h2>
+    <h2 style="color:green">Fishing, hunting, and outdoorsmanship</h2>
 
     <p>
-        <a href="http://allrecipes.com">Allrecipes</a>
-
-
+    	<a href="http://www.bouldercounty.org/os/events/pages/fishing.aspx">Fishing in Boulder</a>
     </p>
     <p>
-        <a href="http://www.epicurious.com">Epicurious</a>
-
-     </p>
-     <p>
-         <a href="http://www.food.com">Food</a>
-
-      </p>
-      <p>
-      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
-      </p>
-
+		<a href="http://cpw.state.co.us/thingstodo/Pages/Hunt.aspx">Hunting in Colorado</a>
+    </p>
+    <p>
+		<a href="http://www.bouldercounty.org/os/parks/Pages/default.aspx">Boulder Parks</a>
+    </p>
+	<p>
+        <a href="paleoplanet69529.yuku.com">Paleoplanet</a>
+    </p>
     
 </div>
 <div class="site-recipes">
-    <h2>Gardening, growing vegetables, and other plant-related activities:</h2>
+    <h2 style="color:green">Gardening, growing vegetables, and other plant-related activities:</h2>
 
     <p>
-        <a href="http://allrecipes.com">Allrecipes</a>
-
-
+        <a href="http://www.garden.org/">National Gardening Association</a>
     </p>
     <p>
-        <a href="http://www.epicurious.com">Epicurious</a>
-
-     </p>
-     <p>
-         <a href="http://www.food.com">Food Network</a>
-
-      </p>
-      <p>
-      <a href="http://www.simplyrecipes.com">Simply Recipes</a>
-      </p>
+    	<a href="https://en.wikipedia.org/wiki/Gardening">Gardening Wiki</a>
+    </p>
+    <p>
+		<a href="http://forums.gardenweb.com/forums">Gardening Forum</a>
+    </p>
+    <p>
+		<a href="http://www.nativeseeds.org/">Native Seeds</a>
+    </p>
 
     
 </div>
